17564043f44edb23beb27ece87bdcf65fda2ad14
Felipe Walther <felipe_walther@hotmail.com>
2019-05-01 16:24:39 -0300

    criado tabela para receber os dados

--- view/index.php
+++ view/index.php
@@ -1,14 +1,36 @@
 <?php
 require_once '../api/apiSearchCep.php';
 
-header("Content-type: text/plain; charset=UTF-8");
-
 $cep = new Cep();
 if (isset($_GET['cep'])) {
     $result = $cep->buscarCep(preg_replace('/[^0-9]/', '', $_GET['cep']));
-	die(json_encode($result ? $result[0] : null));
 } else if (isset($_GET['endereco'])) {
     $result = $cep->buscarCep($_GET['endereco']);
-	die(json_encode($result));
 }
+
+header('Content-type: text/html; charset=UTF-8');
+echo "
+    <style type='text/css'>
+    .tg  {border-collapse:collapse;border-spacing:0;margin:0px auto;}
+    .tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:black;}
+    .tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:black;}
+    .tg .tg-baqh{text-align:center;vertical-align:top}
+    @media screen and (max-width: 767px) {.tg {width: auto !important;}.tg col {width: auto !important;}.tg-wrap {overflow-x: auto;-webkit-overflow-scrolling: touch;margin: auto 0px;}}</style>
+    <div class='tg-wrap'>
+        <table class='tg'>
+            <tr>
+                <th class='tg-baqh'>Localização</th>
+                <th class='tg-baqh'>Dados</th>
+            </tr>";
+            foreach ($result as $values) {
+                foreach ($values as $key => $value){
+                    echo "<tr>
+                        <td class='tg-baqh'>{$key}</td>
+                        <td class='tg-baqh'>{$value}</td>
+                    </tr>";
+                }
+            }
+           echo "
+        </table>
+    </div>";
 ?>
