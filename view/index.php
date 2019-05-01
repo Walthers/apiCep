ed7641d3d0e791bc99fd2f03bf7a7c32ad537e37
Felipe Walther <felipe_walther@hotmail.com>
2019-05-01 17:29:59 -0300

    validações gerais

--- view/index.php
+++ view/index.php
@@ -7,30 +7,50 @@ if (isset($_GET['cep'])) {
 } else if (isset($_GET['endereco'])) {
     $result = $cep->buscarCep($_GET['endereco']);
 }
-
 header('Content-type: text/html; charset=UTF-8');
+
 echo "
-    <style type='text/css'>
-    .tg  {border-collapse:collapse;border-spacing:0;margin:0px auto;}
-    .tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:black;}
-    .tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:black;}
-    .tg .tg-baqh{text-align:center;vertical-align:top}
-    @media screen and (max-width: 767px) {.tg {width: auto !important;}.tg col {width: auto !important;}.tg-wrap {overflow-x: auto;-webkit-overflow-scrolling: touch;margin: auto 0px;}}</style>
-    <div class='tg-wrap'>
-        <table class='tg'>
-            <tr>
-                <th class='tg-baqh'>Localização</th>
-                <th class='tg-baqh'>Dados</th>
-            </tr>";
+<h1>API não oficial para pesquisa de CEP e endereços</h1>
+<p>Desenvolvido em php com host em XAMPP, pesquisando no site oficial dos correios (www.buscacep.correios.com.br)</p>
+
+<h2>Consulta de CEP</h2>
+<p>Envia-se o CEP ou endereço desejado pela url e é retornado os dados em uma tabela. Utilizar um host a sua escolha.</p>
+<p>Exemplo de uso para cep:</p>
+<pre>http://localhost/apiCep/view/index.php?cep=xxxxxxxx</pre>
+<p>Exemplo de uso para endereço:</p>
+<pre>http://localhost/apiCep/view/index.php?endereco=xxxxxxxx</pre>
+<p>Exemplo de uso para endereço composto (separar com _):</p>
+<pre>http://localhost/apiCep/view/index.php?endereco=enrereco_composto</pre>
+<h2>Resultado:</h2>
+";
+
+if (!empty($result)){
+    echo "
+        <style type='text/css'>
+        .tg {border-collapse:collapse;border-spacing:0;margin:10px auto;}
+        .tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:black;}
+        .tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:black;}
+        .tg .tg-baqh{text-align:center;vertical-align:top}
+        .tg-wrap {overflow-x: auto;-webkit-overflow-scrolling: touch;margin: auto 0px;}</style>
+        <div class='tg-wrap'>";
             foreach ($result as $values) {
+                echo "
+                <table class='tg'>
+                    <tr>
+                        <th class='tg-baqh'>Localização</th>
+                        <th class='tg-baqh'>Dados</th>
+                    </tr>";
                 foreach ($values as $key => $value){
-                    echo "<tr>
+                    echo "
+                    <tr>
                         <td class='tg-baqh'>{$key}</td>
                         <td class='tg-baqh'>{$value}</td>
                     </tr>";
                 }
+                echo "</table>";
             }
-           echo "
-        </table>
-    </div>";
+        echo "</div>";
+} else {
+    echo "<h2>CEP/Endereço inválido!</h2>"; 
+}
 ?>
